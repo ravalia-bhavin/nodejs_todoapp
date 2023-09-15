8729f288f060b31ff40345112df172ac61af8e9d
bhavin ravalia <ravaliabhavin01@gmail.com>
2023-09-15 19:10:41 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ export const connectDb = () => {
   mongoose
     // 127.0.0.1 --- 💀💀💀
     .connect(process.env.MONGO_URI, { dbName: "backendapi" })
-    .then(() => console.log("Database Connected ✅ "))
+    .then((c) => console.log(`Database Connected ✅ with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
